9756ac649f09b8bc63572e5434bd34371dccbedf
ALRIZKI HAIKAL <115471452+sipalingtestnet@users.noreply.github.com>
2023-02-13 19:54:30 +0800

    Update coreum1.sh

--- coreum1.sh
+++ coreum1.sh
@@ -81,7 +81,43 @@ curl -LOf https://github.com/CoreumFoundation/coreum/releases/download/v0.1.1/co
 mv $CORE_BINARY_NAME $CORE_HOME/bin/cored
 chmod +x $CORE_HOME/bin/*
 cored version
+# Set peers and seeds
+PEERS="69d7028b7b3c40f64ea43208ecdd43e88c797fd6@34.69.126.231:26656,b2978432c0126f28a6be7d62892f8ded1e48d227@34.70.241.13:26656,7c0d4ce5ad561c3453e2e837d85c9745b76f7972@35.238.77.191:26656,0aa5fa2507ada8a555d156920c0b09f0d633b0f9@34.173.227.148:26656,4b8d541efbb343effa1b5079de0b17d2566ac0fd@34.172.70.24:26656,27450dc5adcebc84ccd831b42fcd73cb69970881@35.239.146.40:26656,5add70ec357311d07d10a730b4ec25107399e83c@5.196.7.58:26656,1a3a573c53a4b90ab04eb47d160f4d3d6aa58000@35.233.117.165:26656,abbeb588ad88176a8d7592cd8706ebbf7ef20cfe@185.241.151.197:26656,39a34cd4f1e908a88a726b2444c6a407f67e4229@158.160.59.199:26656,051a07f1018cfdd6c24bebb3094179a6ceda2482@138.201.123.234:26656,cc6d4220633104885b89e2e0545e04b8162d69b5@75.119.134.20:26656"
+SEEDS="64391878009b8804d90fda13805e45041f492155@35.232.157.206:26656,53f2367d8f8291af8e3b6ca60efded0675ff6314@34.29.15.170:26656"
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$FOLDER/$CHAIN/config/config.toml
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$FOLDER/$CHAIN/config/config.toml
 
+# Download genesis and addrbook
+curl -Ls $GENESIS > $HOME/$FOLDER/$CHAIN/config/genesis.json
+curl -Ls $ADDRBOOK > $HOME/$FOLDER/$CHAIN/config/addrbook.json
+
+# Set Port
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/$FOLDER/$CHAIN/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}317\"%; s%^address = \":8080\"%address = \":${PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}091\"%" $HOME/$FOLDER/$CHAIN/config/app.toml
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$FOLDER/$CHAIN/config/app.toml
+# Variable
+SOURCE=coreum
+WALLET=wallet
+BINARY=cored
+CHAIN=coreum-testnet-1
+FOLDER=.core
+VERSION=v0.1.1
+DENOM=utestcore
+COSMOVISOR=cosmovisor
+REPO=https://github.com/CoreumFoundation/coreum/releases/download/v0.1.1/cored-linux-amd64
+BIN_NAME=cored-linux-amd64
+GENESIS=https://snap.nodexcapital.com/coreum/genesis.json
+ADDRBOOK=https://snap.nodexcapital.com/coreum/addrbook.json
+PORT=52
 # config
 CORE_NODE_CONFIG=$CORE_HOME/config/config.toml
 
@@ -121,4 +157,17 @@ sudo systemctl restart cored && sudo journalctl -u cored -f -o cat
 echo '=============== DONO ==================='
 echo -e 'To check logs: \e[1m\e[32mjournalctl -u cored -f -o cat\e[0m'
 
-
+# Variable
+SOURCE=coreum
+WALLET=wallet
+BINARY=cored
+CHAIN=coreum-testnet-1
+FOLDER=.core
+VERSION=v0.1.1
+DENOM=utestcore
+COSMOVISOR=cosmovisor
+REPO=https://github.com/CoreumFoundation/coreum/releases/download/v0.1.1/cored-linux-amd64
+BIN_NAME=cored-linux-amd64
+GENESIS=https://snap.nodexcapital.com/coreum/genesis.json
+ADDRBOOK=https://snap.nodexcapital.com/coreum/addrbook.json
+PORT=52
